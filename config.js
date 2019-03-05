e9a5ac82e126c7de707a00d8ae3fa64fd8e8400b
lui3244116 <455912839@qq.com>
2019-03-05 10:28:37 +0800

    前端api配置js定义

---
+++ config.js
@@ -0,0 +1,4 @@
+var host = 'http://hyu6209260001.my3w.com'; // 域名
+var api_home = '/public/index/api/home_api'; // 获取首页数据
+var api_about = '/public/index/api/about_api'; // 获取关于我们数据
+var api_project = '/public/index/api/project_api'; // 获取项目数据
