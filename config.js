993970c4e5d95feaf44b2f8f87f64a004cfcfc5e
lui3244116 <455912839@qq.com>
2019-03-05 16:55:23 +0800

    api接口地址配置

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// var host = 'http://hyu6209260001.my3w.com'; // 域名
-var host = 'http://local.yl.com'; // 域名
-var upload_url = '/upload/';
-var api_home = '/index/api/home_api'; // 获取首页数据
-var api_about = '/index/api/about_api'; // 获取关于我们数据
-var api_project = '/index/api/project_api'; // 获取项目数据
-var api_active_type = '/index/api/active_type_api'; // 获取公司动态类型
+var host = 'http://hyu6209260001.my3w.com'; // 域名
+// var host = 'http://local.yl.com'; // 域名
+var upload_url = '/public/upload/';
+var api_home = '/public/index/api/home_api'; // 获取首页数据
+var api_about = '/public/index/api/about_api'; // 获取关于我们数据
+var api_project = '/public/index/api/project_api'; // 获取项目数据
+var api_active_type = '/public/index/api/active_type_api'; // 获取公司动态类型
